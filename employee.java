58990b1460873f540adcaaf4a7023613889c7bff
bachunithinkumar <153371611+bachunithinkumar@users.noreply.github.com>
2025-11-04 19:35:40 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,127 @@
+//1.
+
+/*public class employee{
+
+    String empName;
+    String empId;
+    double empSalary;
+
+    public void adddetails(String name, String id, double salary)
+             {
+               empName = name;
+               empId = id;
+               empSalary = salary;
+                   }
+
+        public String details()
+           {
+              return "Employee Name"+ empName+"\n"+
+                     "Employee ID"+ empId+"\n"+
+                     "Employee Salary"+ empSalary;
+          }
+  
+                       public static void main(String[] args)
+       {
+                    employee obj=new employee();
+                     obj.adddetails("john doe","E123",5000.0);
+                     System.out.println(obj.details());   
+                     }
+}*/
+
+
+//2.
+
+/*
+public class employee{
+
+       String empCar;
+       String empModel;
+       int empYear;
+
+         public void adddetails(String car, String model, int year){
+ 
+            empCar = car;
+            empModel = model;
+            empYear = year;
+}
+         public String details()
+      {
+     return   "Employee Car :"+ empCar +
+              "Employee Model :"+ empModel +
+              "Employee Year :"+ empYear ; 
+              
+}                
+                 public static void main(String[] args){
+
+                     employee obj= new employee();
+                     obj.adddetails("Toyota","Hilux", 2025);
+                      System.out.println(obj.details()); 
+       }
+                  
+}*/
+
+//3.
+
+/*public class employee{
+
+         String empBooktitle;
+         String empAuthor;
+         long empIsbn;
+            
+            public void adddetails(String booktitle, String author, long isbn){
+
+               empBooktitle = booktitle;
+               empAuthor = author;
+               empIsbn = isbn;
+}
+                 public String details()
+        {
+                     return "Employee Booktitle :"+ empBooktitle+
+                             "Employee Author :"+ empAuthor +
+                             "Employee Isbn :"+ empIsbn ; 
+                     }
+                public static void main(String[] args)
+{       
+                    employee obj=new employee();
+       
+                       obj.adddetails("Belive in your self","DR.Joseph murphy", 9788197);
+                      System.out.println(obj.details()); 
+                    
+
+                  }
+                  
+
+}*/
+
+//4.
+
+/*public class employee{
+
+         String empName;
+         int empId;
+         int empPrice;
+            
+            public void adddetails(String name, int id, int price){
+
+               empName = name;
+               empId = id;
+               empPrice = price;
+}
+                 public String details()
+        {
+                     return "Employee Name :"+ empName+
+                             "Employee Id :"+ empId +
+                             "Employee Price :"+ empPrice ; 
+                     }
+                public static void main(String[] args)
+{       
+                    employee obj=new employee();
+       
+                       obj.adddetails("Laptop",90341, 59999);
+                      System.out.println(obj.details()); 
+                    
+
+                  }
+                  
+
+}*/
