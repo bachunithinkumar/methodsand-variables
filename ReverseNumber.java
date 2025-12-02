f5a7422cee88c68dab5307e3933d9e230fe72572
bachunithinkumar <153371611+bachunithinkumar@users.noreply.github.com>
2025-12-02 19:18:30 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,49 @@
+/*class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 721;
+        int rev = 0;
+
+        while (num > 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        System.out.println(rev);   // Output: 127
+    }
+}*/
+
+/*
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		
+		int num=1322,sum=0;
+		while(num!=0) {
+			int n=num%10;
+			sum=sum+n;
+			num/=10;
+		}
+		System.out.println(sum);
+		
+
+	}
+
+}
+
+*/
+
+/*
+class ReverseNumber  {
+    public static void main(String[] args) {
+        int num = 4;
+        int fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        System.out.println(fact);   // Output: 24
+    }
+}
+
+*/
